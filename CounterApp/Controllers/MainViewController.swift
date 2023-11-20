b47715387ebb27c657c3d88f0224a2c644c2e8f3
Tolmach <danieloreshenkov@yandex.ru>
2023-11-20 21:30:06 +1000

    удалил не нужные комментария, добавил логику автоскролла в UITextVeiw, добавил private @IBOutlet @IBAction и enum Description,

--- CounterApp/Controllers/MainViewController.swift
+++ CounterApp/Controllers/MainViewController.swift
@@ -7,20 +7,20 @@
 
 import UIKit
 
-enum Description: String {
+private enum Description: String {
     case numberPlusOne = "значение изменено на +1\n"
     case numberMinusOne = "значение изменено на -1\n"
     case numbermenoDizero = "попытка уменьшить значение счётчика ниже 0\n"
     case numberRestart = "значение сброшено"
 }
 
-class MainViewController: UIViewController {
-
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var restartButton: UIButton!
-    @IBOutlet weak var textView: UITextView!
+class MainViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var restartButton: UIButton!
+    @IBOutlet private weak var textView: UITextView!
     
     private var counter: Int {
         get {
@@ -42,24 +42,39 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         configure()
     }
-
-    @IBAction func addNumberByOne(_ sender: Any) {
-        counterLabel.text = String(counter + 1)
-        textView.text += "\(dateString)\n" + Description.numberPlusOne.rawValue
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
     
-    @IBAction func subtractNumberByOne(_ sender: Any) {
+    @IBAction private func addNumberByOne(_ sender: Any) {
+        counterLabel.text = String(counter + 1)
+        textView.text += "\(dateString)\n" + Description.numberPlusOne.rawValue
+        let range = NSRange(location: textView.text.count - 1, length: 0)
+        textView.scrollRangeToVisible(range)
+        scrollAutoTextView()
+    }
+    
+    @IBAction private func subtractNumberByOne(_ sender: Any) {
         if counter > 0 {
             counterLabel.text = String(counter - 1)
             textView.text += "\(dateString)\n" + Description.numberMinusOne.rawValue
+            scrollAutoTextView()
         } else {
             textView.text += "\(dateString)\n" + Description.numbermenoDizero.rawValue
+            scrollAutoTextView()
         }
     }
     
-    @IBAction func restartCounter(_ sender: Any) {
+    @IBAction private func restartCounter(_ sender: Any) {
         counterLabel.text = "0"
         textView.text += "\(dateString)\n" + Description.numberRestart.rawValue
+        scrollAutoTextView()
+    }
+    
+    private func scrollAutoTextView() {
+        let range = NSRange(location: textView.text.count, length: 0)
+        textView.scrollRangeToVisible(range)
     }
     
     private func configure() {
